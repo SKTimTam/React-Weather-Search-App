d271a354c2dece4014289d71c86d0927d83be05f
Geon Park <timp0303@gmail.com>
2024-03-31 23:41:18 +1100

    boom

---
+++ src/types/WeatherRequests.tsx
@@ -0,0 +1,60 @@
+
+interface locationDetails {
+    name: String,
+    region: String,
+    country: String,
+    lat: Number,
+    lon: Number,
+    tz_id: String,
+    localtime_epoch: Number,
+    localtime: String,
+};
+
+interface currentDetails {
+    last_updated_epoch: Number,
+    last_updated: String,
+    temp_c: Number,
+    temp_f: Number,
+    is_day: Number,
+    condition: {
+        [key: string]: conditionDetails
+    },
+    wind_mph: Number,
+    wind_kph: Number,
+    wind_degree: Number,
+    wind_dir: String,
+    pressure_mb: Number,
+    pressure_in: Number,
+    precip_mm: Number,
+    precip_in: Number,
+    humidity: Number,
+    cloud: Number,
+    feelslike_c: Number,
+    feelslike_f: Number,
+    vis_km: Number,
+    vis_miles: Number,
+    uv: Number,
+    gust_mph: Number,
+    gust_kph: Number
+};
+
+interface conditionDetails {
+    text: String,
+    icon: String,
+    code: Number,
+};
+
+export interface WeatherRequestResponse {
+    data: WeatherRequests[];
+}
+
+interface WeatherRequests {
+
+    location: {
+        [key: string]: locationDetails
+    };
+    current: {
+        [key: string]: currentDetails
+    }  
+};
+
