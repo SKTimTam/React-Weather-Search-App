31c7693217a6a5e173f53636aea663f24d5156f3
Geon Park <timp0303@gmail.com>
2024-05-28 16:22:38 +1000

    added a map and a history with additional card element

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,86 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import { WeatherRequestResponse } from './types/WeatherRequests';
 import { Input } from './components/ui/input';
 import { Button } from './components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
 import { getDataApi } from './services/DataAPI';
+import { Header } from './components/ui/header';
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
+import WeatherCard  from './components/ui/weatherCard'
+
+
 
 
   function App() {
 
   const [weatherData,setWeatherData] = useState<WeatherRequestResponse>();
   const [city, setCity] = useState('');
+  const [history, setHistory] = useState([''])
+  const [center, setCenter] = useState<[number, number] | undefined>([50,50])
 
-  useEffect(()=>{
-    localStorage.setItem('cities', JSON.stringify(city));
-  },[city])
   
-  console.log(localStorage)
+  function ChangeMapView({center}: any) {
+    console.log(center);
+    const map = useMap();
+    map.setView(center, map.getZoom());
+    return null;
+  }
 
+
+
+
+  const handleButtonClick = () => {
+    getDataApi(city,setWeatherData);
+    setHistory((history)=> [...history, city]);
+    if (weatherData) {
+      setCenter([weatherData.location.lat,weatherData.location.lon])
+    }
+  }
+
+  console.log(history)
+  console.log(weatherData)
+  console.log(center)
+
+  
   return (
     <>
-      <div className="flex-container" style={{display: 'flex'}} >
+      <div className="flex-container" style={{display: 'flex', justifyContent: 'flex-start'}} >
+        <Header/>
+      </div>
+      <br/>
+      <div className="flex-container" style={{display: 'flex', justifyContent: 'flex-start'}} >
         <Input className="w-[300px]" type="string" placeholder="City Name" onChange={(e)=>setCity(e.target.value)}/>
-        <Button className="w-[50px]" type="submit" onClick={()=>getDataApi(city, setWeatherData)}>Search</Button>
+        <Button className="w-[50px]" type="submit" onClick={handleButtonClick}>Search</Button>
       </div>
       <div className="flex-container" style={{display: 'flex', flexDirection: 'column'}} >
-        <br></br>
+        <br/>
           {weatherData ? (
-            <Card className="w-[350px]">
-              <CardHeader>
-                <CardTitle> City: {weatherData.location.name}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>Temperature is: {weatherData.current.temp_c} Celsius</p>
-                <p>Local time is: {weatherData.location.localtime}</p>
-                <p>Condition is : {weatherData.current.condition.text}</p>
-                <p>Latitude is : {weatherData.location.lat}</p>
-                <p>Longtitude is : {weatherData.location.lon}</p>
-              </CardContent>
-            </Card>
+            <>
+              <div className="flex-container" style={{display: 'flex', flexDirection: 'row'}}>
+                <WeatherCard data={weatherData} historyData={history}/>
+              </div>
+              <br/>
+                <MapContainer center={[weatherData.location.lat,weatherData.location.lon]} zoom={13} scrollWheelZoom={false}>
+                  <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={[weatherData.location.lat,weatherData.location.lon]}/>
+                  <ChangeMapView center={[weatherData.location.lat,weatherData.location.lon]}/>
+                </MapContainer>
+            </>
+            
           ) : 
-            <Card className="w-[350px]">
-              <CardHeader>
-                <CardTitle> Enter in a City Name please</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p> Try your favourite City!</p>
-              </CardContent>
-            </Card>}
+          <Card className="w-[350px]">
+            <CardHeader>
+              <CardTitle> Enter in a city name</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p> Try your favourite City!</p>
+            </CardContent>
+          </Card>}
       </div>
+
     </>
   );
 };
--- src/services/DataAPI.tsx
+++ src/services/DataAPI.tsx
@@ -11,5 +11,18 @@ export const getDataApi = (city:string, setWeatherData: Dispatch<SetStateAction<
     return axios.get<WeatherRequestResponse>(`http://api.weatherapi.com/v1/current.json?key=${token}&q=${city}`)
     .then ((response)=>{
       setWeatherData(response.data);
-    })
+    }).catch((error)=>{
+    
+      if (error.response) { // status code out of the range of 2xx
+        console.log("Data :" , error.response.data);
+        console.log("Status :" + error.response.status);
+        return error.response
+      } else if (error.request) { // The request was made but no response was received
+        console.log(error.request);
+        return error.response
+      } else {// Error on setting up the request
+        console.log('Error', error.message);
+        return error.response 
+      }
+  });
   };
